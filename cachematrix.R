cef6f674649d79c49dc7a37272867961541c1cb0
courseragit <courseragit@users.noreply.github.com>
2016-01-23 11:37:22 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+# In order to test these functions, exeute the following steps after compiling each:
+# Create a seed matrix
+# > x <- matrix(rnorm(18), nrow = 6) 
+# Use makeCacheMatrix to create a new matrix
+# > y <- makeCacheMatrix(x) 
+# Execut cacheSolve to return the inverse of the above matrix and place in cache.
+# > cacheSolve(y)  
+# Execute again. Upon second and subsequent executions, you should see the message "returning cached information".
+# > cacheSolve(y)                           
+# > cacheSolve(y)                                             
 
-## Write a short comment describing this function
-
+# makeCacheMatrix
 makeCacheMatrix <- function(x = matrix()) {
+    # inv will store the cached inverse matrix
+    inv <- NULL
 
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    
+    # Return
+    list(set = set, 
+         get = get, 
+         setinv = setinv, 
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# This function computes the inverse of the matrix. If the inverse has already
+# been calculated, the function returns the cached value rather than re-calculating.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    
+    # Test to see if we have already cached
+    if (!is.null(inv)) {
+        message("returning cached information")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    
+    x$setinv(inv)
+
+    inv
 }
